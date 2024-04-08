b188988b8bbb6ef5f7cdea15e7fe198f9422cdd0
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2024-04-09 00:20:00 +0300

    feat : initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const { Buffer } = require('buffer');
+const { config } = require('dotenv');
+config();
+
+const app = express();
+
+app.use('/image/:imageName', async (req, res) => {
+	try {
+		const { imageName } = req.params;
+		const imageUrl =
+			process.env.MAIN_SERVER + `/document/photos_couvert/${imageName}`;
+		const response = await fetch(imageUrl);
+		const arrayBuffer = await response.arrayBuffer();
+		const buffer = Buffer.from(arrayBuffer);
+		res.writeHead(200, {
+			'Content-Type': 'image/png',
+			'Access-Control-Allow-Origin': '*',
+		});
+		res.end(buffer);
+	} catch (error) {
+		console.error('Error fetching image:', error);
+		res.status(500).send('Internal Server Error');
+	}
+});
+
+app.listen(process.env.PORT, () => {
+	console.log(`Proxy server running on port`);
+});
